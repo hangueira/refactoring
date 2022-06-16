ce0090707167492251320a8311b711cbd320e806
hangueira <hks713@naver.com>
2022-06-16 15:01:25 +0900

    변수 캡슐화 하기

--- ch6/6-6-use.js
+++ ch6/6-6-use.js
@@ -1,6 +1,7 @@
 import { getDefaultOwner } from './6-6.js';
 
 const owner = getDefaultOwner();
-owner.firstName = '엘리';
-console.log(owner);
+console.log(owner.firstName);
+owner.changeFirstName('hks');
+console.log(owner.firstName);
 console.log(getDefaultOwner());
--- ch6/6-6.js
+++ ch6/6-6.js
@@ -1,4 +1,27 @@
-let defaultOwner = { firstName: '마틴', lastName: '파울러' };
+class Person {
+  #firstName;
+  #lastName;
+
+  constructor(name) {
+    this.#firstName = name.firstName;
+    this.#lastName = name.lastName;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+
+  get lastName() {
+    return this.#lastName;
+  }
+
+  changeFirstName(firstName) {
+    this.#firstName = firstName;
+    return this.#firstName;
+  }
+}
+
+let defaultOwner = new Person({ firstName: '마틴', lastName: '파울러' });
 
 export function getDefaultOwner() {
   return defaultOwner;
